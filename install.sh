def9a4847b9fa4d1d095e2d523ddad8b1cd8bf9d
Cisco Ramon <cisssco581b@gmail.com>
2024-08-01 11:50:24 +0530

    Updated on Aug-01 at 11:50

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ install(){
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
     cd ~
-    git clone https://github.com/CISSSCO/myvimrc_config.git
     if [ -f .vimrc ]; then
         mv .vimrc .vimrc.old
     fi
